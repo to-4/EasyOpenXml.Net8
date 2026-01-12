f876afb174a2a1f28ac81f6bea655b3a4f105037
to-4 <to-4@example.com>
2026-01-12 15:59:50 +0900

    feat: add console app for testing class library

---
+++ EasyOpenXml.Excel.DemoConsole/Demos/Demo01_CreateNew.cs
@@ -0,0 +1,18 @@
+﻿using EasyOpenXml.Excel;
+
+namespace EasyOpenXml.Excel.DemoConsole.Demos;
+
+internal static class Demo01_CreateNew
+{
+    public static void Run()
+    {
+        var path = Paths.OutFile("demo01_new.xlsx");
+
+        //using var excel = ExcelDocument.Create(path);
+        //excel.SetValue("A1", "Hello");
+        //excel.SetValue("B1", "World");
+        //excel.Save();
+
+        Console.WriteLine("Created: " + path);
+    }
+}
