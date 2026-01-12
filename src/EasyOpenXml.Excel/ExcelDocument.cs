acd259dff2c240909cd2e19d239f10a62660e5ff
to-4 <to-4@example.com>
2026-01-12 21:19:29 +0900

    feat: add InitializeFile, SetValue, etc

--- src/EasyOpenXml.Excel/ExcelDocument.cs
+++ src/EasyOpenXml.Excel/ExcelDocument.cs
@@ -1,10 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using EasyOpenXml.Excel.Internals;
 
 namespace EasyOpenXml.Excel
 {
-    internal class ExcelDocument
+    public sealed class ExcelDocument : IDisposable
     {
+        private ExcelInternal _internal = new ExcelInternal();
+        private bool _disposed;
+
+        public void InitializeFile(string path1, string path2 = "")
+        {
+            _internal = new ExcelInternal();
+
+            var result = _internal.OpenBook(path1, path2);
+            if (result < 0)
+            {
+                throw new ExcelDocumentException("Failed to open Excel file.");
+            }
+        }
+
+        public void SheetSelect(int sheetId)
+        {
+            EnsureNotDisposed();
+            _internal.SheetNo = sheetId;
+        }
+
+        public IReadOnlyList<string> SheetNames
+        {
+            get
+            {
+                EnsureNotDisposed();
+                return _internal.SheetNames;
+            }
+        }
+
+        public void SetValue(int sx, int sy, object value)
+        {
+            SetValue(sx, sy, sx, sy, value);
+        }
+
+        public void SetValue(int sx, int sy, int ex, int ey, object value)
+        {
+            EnsureNotDisposed();
+            var pos = _internal.Pos(sx, sy, ex, ey);
+            pos.Value = value;
+        }
+
+        public void FinalizeFile(bool save = true)
+        {
+            EnsureNotDisposed();
+            _internal.CloseBook(save);
+        }
+
+        private void EnsureNotDisposed()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(ExcelDocument));
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+            _internal?.Dispose();
+            _disposed = true;
+        }
     }
 }
--- src/EasyOpenXml.Excel/Internals/ExcelInternal.cs
+++ src/EasyOpenXml.Excel/Internals/ExcelInternal.cs
@@ -1,10 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using DocumentFormat.OpenXml.Packaging;
+using EasyOpenXml.Excel.Models;
 
 namespace EasyOpenXml.Excel.Internals
 {
-    internal class ExcelInternal
+    internal sealed class ExcelInternal : IDisposable
     {
+        private SpreadsheetDocument _document;
+        private SheetManager _sheetManager;
+        private bool _opened;
+
+        internal int OpenBook(string strFileName, string strOverlay)
+        {
+            try
+            {
+                // 1. Open Excel file
+                _document = SpreadsheetDocument.Open(strFileName, true);
+
+                // 2. Initialize sheet manager
+                _sheetManager = new SheetManager(_document);
+
+                _opened = true;
+                return 0;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        internal void CloseBook(bool mode)
+        {
+            if (!_opened) return;
+
+            if (mode)
+            {
+                _document.Save();
+            }
+
+            Dispose();
+        }
+
+        internal int SheetNo
+        {
+            set => _sheetManager.SelectByIndex(value);
+        }
+
+        internal IReadOnlyList<string> SheetNames
+            => _sheetManager.GetSheetNames();
+
+        internal Pos Pos(int sx, int sy)
+            => Pos(sx, sy, sx, sy);
+
+        internal Pos Pos(int sx, int sy, int ex, int ey)
+        {
+            var proxy = new PosProxy(
+                _document,
+                _sheetManager.CurrentWorksheetPart,
+                sx, sy, ex, ey);
+
+            return new Pos(proxy);
+        }
+
+        public void Dispose()
+        {
+            _document?.Dispose();
+            _document = null;
+            _opened = false;
+        }
     }
 }
